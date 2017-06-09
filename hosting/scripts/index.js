8ebc104f47af53de8e90d25cd7f15141dae18e1d
Alexander Valencia <alexander.valencia@liferay.com>
2017-06-09 13:43:07 -0700

    Adds parsing for scss file

--- hosting/scripts/index.js
+++ hosting/scripts/index.js
@@ -1,12 +1,44 @@
 $(function() {
+	var REGEX = /^\$([\w\-]+): ([^!]+) !default;/g;
 	var customizer = {
-		readScss: function() {
+
+		formatData: function(data) {
+			var instance = this;
+
+			instance.sassArray = [];
+
+			data.forEach(
+				function(line) {
+					var res = REGEX.exec(line);
+
+					if (!!res) {
+						console.log(res);
+
+						console.log(res[1] + ' ' + res[2]);
+
+						instance.sassArray.push({
+							'variable': 'hi',
+							'value': 'bye'
+						});
+					}
+				}
+			)
+
+			console.log(instance.sassArray);
+		},
+
+		readFile: function() {
+			var instance = this;
+
 			$.when($.get('assets/_variables.scss')).done(
-				function(res) {
-					console.log(res);
+				function(data) {
+					data = data.split('\n');
+					console.log(data);
+					instance.formatData(data);
 				}
 			);
 		}
 	}
 
+	customizer.readFile();
 });
